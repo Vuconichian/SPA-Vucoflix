fd33a911c288807016994bbae718b0d7c95a4af9
UADER-IS2-VUCONICH <ianvuconich@gmail.com>
2024-11-15 21:17:47 -0300

    Final changes

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -2,9 +2,7 @@ const API_KEY = '759cd8efe50cfc43465c1967c42854df';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const tmdbAPI = {
-    getNewMovies: () => 
-        fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=es-ES`)
-            .then(res => res.json()),
+
     getPopularMovies: (page = 1) => 
         fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=${page}`)
             .then(res => res.json()),
